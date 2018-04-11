5de783e245a4f2e6ff58a7d2b243cac7995a3fe3
Michael Blandford <mdblandford@gmail.com>
2018-04-11 14:03:08 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog 
+  def name=(this_dog_name)
+    this_dogs_name = dog_name
+  end
+ 
+  def name
+    @name
+  end
+  
+  def name=(new_dog_name)
+    @name = new_dog_name
+  end
+  def breed=(dog_breed)
+    this_dogs_breed = dog_breed
+  end
+ 
+  def breed
+    @breed 
+  end
+  
+  def breed=(new_dog_breed)
+    @breed = new_dog_breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,24 @@
+class Person 
+  def name=(this_person_name)
+    this_person_name = person_name
+  end
+ 
+  def name
+    @name
+  end
+  
+  def name=(new_person_name)
+    @name = new_person_name
+  end
+  def job=(person_job)
+    this_person_breed = person_breed
+  end
+ 
+  def job
+    @job 
+  end
+  
+  def job=(new_person_job)
+    @job = new_person_job
+  end
+end
