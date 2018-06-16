94bddfea63a148a548edd8bedea0df0e77da1123
Michael Blandford <mdblandford@gmail.com>
2018-06-16 15:05:50 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,17 @@
-class Dog 
+class Dog
   def name=(this_dog_name)
     @name = this_dog_name
   end
- 
+
   def name
     @name
   end
-  
+
   def breed=(this_dog_breed)
     @breed = this_dog_breed
   end
- 
+
   def breed
-    @breed 
+    @breed
   end
 end
